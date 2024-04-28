0a55c57a839ad84e3b3d6d0c8e00636afd513695
unknown <111880@tecnicatura.frc.utn.edu.ar>
2024-04-28 01:40:37 -0300

    1er commit

---
+++ Unidad 2/Clase 19-04/1erJavaScript.js
@@ -0,0 +1,36 @@
+// console.log("mi primer js")
+
+// let today=new Date()
+// console.log(today)
+
+let dd=today.getDate()
+let mm=today.getMonth()+1
+let yyyy=today.setFullYear()
+
+// console.log(dd+"/"+mm+"/"+yyyy)
+
+console.log(`${dd}/${mm}/${yyyy}`)
+
+function suma(a,b)
+{
+    if(a===b)
+    {
+        return (a+b)
+    }
+    else
+    {
+        return (a*b)
+    }
+}
+sumar2= function(a,b)
+{
+    return (a+b)
+}
+
+sumar3=(a,b)=>{return (a+b)}
+
+//en java script una funcion es un objeto!!!!
+
+
+//"node" para ejecutar en terminal
+
